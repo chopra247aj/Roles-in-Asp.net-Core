62304bf7c63c5fba621a24371cf77dbb08c1a7dd
Ajay Chopra <ajaychopra156@gmail.com>
2021-05-21 17:29:15 +0530

    Add project files.

---
+++ Data/Migrations/20210408114124_addMovies.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Data.Migrations
+{
+    public partial class addMovies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: false),
+                    BoxOffice = table.Column<string>(nullable: false),
+                    Active = table.Column<bool>(nullable: false),
+                    DateOfLaunch = table.Column<DateTime>(nullable: false),
+                    Genre = table.Column<string>(nullable: false),
+                    HasTeaser = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string BoxOffice { get; set; }
+
+        [Required]
+        [Display(Name = "Is Movie Active?")]
+        public bool Active { get; set; }
+
+        [Display(Name = "Date of Launch")]
+        public DateTime DateOfLaunch { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Display(Name = "Movie Has Teaser?")]
+        public bool HasTeaser { get; set; }
+
+    }
+}
